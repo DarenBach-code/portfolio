d916ac92ef25a9c293b21728dec5d9bc8663e750
Daren Bach <darenbach2020@gmail.com>
2025-02-07 11:00:22 -0600

    fixed for big screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-w-screen min-w-screen bg-slate-900">{children}</body>
+      <body className="min-w-screen min-h-screen bg-gray-800">
+        <div className="flex justify-self-center max-w-[1512px] min-h-screen bg-slate-900">{children}</div>
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ export default {
   		md: '768px',
   		lg: '1024px',
   		xl: '1280px',
-  		'2xl': '1536px'
+  		'2xl': '1536px',
+		mxwidth : '1277px'
   	},
   	extend: {
   		colors: {

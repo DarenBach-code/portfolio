f8b3582373c92669916615946adf900cd1618e1e
Daren Bach <darenbach2020@gmail.com>
2025-02-07 19:32:10 -0600

    completed for all screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-w-screen min-h-screen bg-gray-800">
-        <div className="flex justify-self-center max-w-[1512px] min-h-screen bg-slate-900">{children}</div>
+        <div className="mxwidth:flex bg-slate-900 min-w-screen mxwidth:max-w-[1512px] mxwidth:justify-self-center min-h-screen">{children}</div>
       </body>
     </html>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ export default {
   		lg: '1024px',
   		xl: '1280px',
   		'2xl': '1536px',
-		mxwidth : '1277px'
+		mxwidth : '1512px'
   	},
   	extend: {
   		colors: {
